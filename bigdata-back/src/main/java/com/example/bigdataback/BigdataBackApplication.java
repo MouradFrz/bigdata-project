4448c03547861b3f17b3074b4f87c5f0e97b7bba
ferhat-hachemi <hachemi.ferhat25@gmail.com>
2024-12-05 23:28:53 +0100

    Initialize backend project

---
+++ bigdata-back/src/main/java/com/example/bigdataback/BigdataBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigdataback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdataBackApplication.class, args);
+    }
+
+}
