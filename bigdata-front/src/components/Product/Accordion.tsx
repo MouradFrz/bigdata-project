e923080af0689379951baba630ce2cdd68fe31b5
Mourad <mouradyaou2001@gmail.com>
2025-01-01 11:09:30 +0100

    minor changes

--- bigdata-front/src/components/Product/Accordion.tsx
+++ bigdata-front/src/components/Product/Accordion.tsx
@@ -3,6 +3,7 @@ import AnimateHeight from 'react-animate-height';
 import { useState } from 'react';
 import { Review } from '../../types';
 import { displayStars } from '../Index/ProductCard';
+import Checkmark from '../../../public/checkmark.svg';
 function Accordion({ reviews }: { reviews: Review[] }) {
     const [active1, setActive1] = useState<string>('1');
     const togglePara1 = (value: string) => {
@@ -16,13 +17,13 @@ function Accordion({ reviews }: { reviews: Review[] }) {
                 return (
                     <div className="border border-[#d3d3d3] dark:border-[#3b3f5c] rounded font-semibold">
                         <div className="border-b h-min border-[#d3d3d3] dark:border-[#3b3f5c]">
-                            <button type="button" className={` p-4 w-full flex items-center text-dark dark:bg-[#1b2e4b]`} onClick={() => togglePara1(String(index))}>
+                            <button type="button" className={` p-4 w-full flex items-center text-dark dark:text-dark-light`} onClick={() => togglePara1(String(index))}>
                                 <div className="flex justify-between w-full">
                                     <div className="flex">
                                         <p>{review.title}</p>
                                         <span className="flex ms-4">{displayStars(review.rating)}</span>
                                     </div>
-                                    <div>{review.verified_purchase ? 'checked' : 'Not Verified'}</div>
+                                    <div>{review.verified_purchase && <img className="w-5" src={Checkmark} />}</div>
                                 </div>
                                 <div className={` ltr:ml-auto rtl:mr-auto`}></div>
                             </button>
--- bigdata-front/src/components/Product/ProductCard.tsx
+++ bigdata-front/src/components/Product/ProductCard.tsx
@@ -11,11 +11,15 @@ function truncateText(text: string, maxLength: number): string {
 
 function ProductCard({ product }: { product: Product }) {
     return (
-        <div className="flex flex-col items-center shadow-md p-4  mb-10 bg-white">
-            <img src={product.images[0].large} alt="" className=" w-full aspect-square" />
-            <h1 className="mt-2">{truncateText(product.title, 70)}</h1>
-            <div className="flex mt-2 w-full">{displayStars(product.averageRating)}</div>
-            <p className="text-xl text-red-600 font-extrabold w-full mt-2">{product.price ? product.price + ' $' : 'Price unavailable'} </p>
+        <div className="flex flex-col justify-between dark:bg-black shadow-md p-4 h-[calc(100%-2rem)]  mb-10 bg-white">
+            <div>
+                <img src={product.images[0].large} alt="" className=" w-full aspect-square" />
+                <h1 className="mt-2">{truncateText(product.title, 70)}</h1>
+            </div>
+            <div className="">
+                <div className="flex mt-2 w-full">{displayStars(product.averageRating)}</div>
+                <p className="text-xl text-red-600 font-extrabold w-full mt-2">{product.price ? product.price + ' $' : 'Price unavailable'} </p>
+            </div>
         </div>
     );
 }
