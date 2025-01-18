f378525d1131df639351194f7e156f1f28453b7d
Mourad <mouradyaou2001@gmail.com>
2025-01-18 16:44:07 +0100

    removed useless graph and added front for another graph

--- bigdata-front/src/pages/AnalyticsReferencePage/availableMetrics.ts
+++ bigdata-front/src/pages/AnalyticsReferencePage/availableMetrics.ts
@@ -1,4 +1,5 @@
 import { MetricSchema } from '../../types';
+import RatingDistributionHistogramme from '../Metric/Metrics/RatingDistributionHistogramme';
 import TopRatedHistogramme from '../Metric/Metrics/TopRatedHistogramme';
 
 export const AVAILABLE_METRICS: MetricSchema[] = [
@@ -7,5 +8,12 @@ export const AVAILABLE_METRICS: MetricSchema[] = [
         title: 'Top rated products',
         description: 'Un histogramme qui represente les 10 produits les mieux note dans le systeme et avec un nombre minimum de review de 60000',
         component: TopRatedHistogramme,
+    },
+    {
+        id: 2,
+        title: 'Rating distribution',
+        description:
+            "Un histogramme qui représente la distribution des notes moyennes (de 1 à 5 étoiles) de tous les produits dans le système, permettant de visualiser le nombre de produits pour chaque niveau de notation et d'identifier les tendances générales de satisfaction client.",
+        component: RatingDistributionHistogramme,
     },
 ];
